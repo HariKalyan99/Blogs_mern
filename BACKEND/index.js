3a42447f8a1c99e0fe94bf54509c894d9a0429e2
HariKalyan99 <harikalyan99tronics@gmail.com>
2024-06-06 12:06:41 +0530

    mern_blogs backend complete

---
+++ BACKEND/index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const port = 8081;
+const express = require('express');
+const mongoUrl = require('./config');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/auth.routes');
+
+const app = express();
+
+
+app.use(express.json());
+app.use("/auth", authRoutes);
+// app.use("/blogs", blogsRouter);
+
+
+mongoose.connect(mongoUrl.mongoDBuri).then(() => {
+    console.log("Successfully connected to the mongo DB");
+    app.listen(port, () => {
+        console.log(`Listening on port: ${port}`)
+    })
+}).catch(error => console.log("Connection unsuccessfull", error))
+
